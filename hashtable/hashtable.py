358a336c3dcb341e20762bd116ed87a80c4d2917
Brianna Workman <brianna.workman@hotmail.com>
2020-05-04 19:46:11 -0400

    fixes get function and adds resize function

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -13,57 +13,51 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
-        self.capactiy = capacity
-        self.storage = [None] * capacity
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
 
     def fnv1(self, key):
         """
         FNV-1 64-bit hash function
-
         Implement this, and/or DJB2.
         """
+        h = 14695981039346656037
+        for b in str(key).encode():
+            h *= 1099511628211
+            h ^= b
+        return h
 
     def djb2(self, key):
         """
         DJB2 32-bit hash function
-
         Implement this, and/or FNV-1.
         """
-        hash_value = 5382
-        for c in key:
-            hash_value = (hash_value * 33) + ord(c)
-        return hash_value
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        # return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        # return self.djb2(key) % self.capacity
+        return self.fnv1(key) % self.capacity
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
         index = self.hash_index(key)
-        self.storage[index] = value
+        self.storage[index] = HashTableEntry(key, value)
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         index = self.hash_index(key)
@@ -72,47 +66,48 @@ class HashTable:
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
         index = self.hash_index(key)
-        return self.storage[index]
+        if self.storage[index] == None:
+            return None
+        else:
+            return self.storage[index].value
 
     def resize(self):
         """
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Implement this.
         """
+        old_storage = self.storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity
+        for i in old_storage:
+            if i == None:
+                continue
+            else:
+                self.put(i.key, i.value)
 
 
 if __name__ == "__main__":
     ht = HashTable(2)
-
     ht.put("line_1", "Tiny hash table")
     ht.put("line_2", "Filled beyond capacity")
     ht.put("line_3", "Linked list saves the day!")
-
     print("")
-
     # Test storing beyond capacity
     print(ht.get("line_1"))
     print(ht.get("line_2"))
     print(ht.get("line_3"))
-
     # Test resizing
     old_capacity = len(ht.storage)
     ht.resize()
     new_capacity = len(ht.storage)
-
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
     # Test if data intact after resizing
     print(ht.get("line_1"))
     print(ht.get("line_2"))
     print(ht.get("line_3"))
-
     print("")
