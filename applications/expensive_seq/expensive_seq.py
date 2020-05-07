597f14e004bdac01e858227fb46ecf7d86d96f45
Brianna Workman <brianna.workman@hotmail.com>
2020-05-07 14:58:56 -0400

    expensive_seq complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,5 +1,17 @@
+cache = {}
+
+
 def expensive_seq(x, y, z):
     # Implement me
+    if x <= 0:
+        return y + z
+
+    if i not in cache and x > 0:
+        cache[i] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y +
+                                                                  2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+
+    return cache[i]
+
 
 if __name__ == "__main__":
     for i in range(10):
